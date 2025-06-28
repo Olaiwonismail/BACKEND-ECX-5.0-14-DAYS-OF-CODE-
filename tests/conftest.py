33beb9dfe10463a5396d83ee5bb6744fc637d6b9
Olaiwonismail <olaiwonismail@gmail.com>
2025-06-28 14:25:46 +0100

    added tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,10 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.main import app
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from main import app
 from app.database import Base, get_db
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
